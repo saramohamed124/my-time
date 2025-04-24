e200caf79722dc3f97aec1e8198482d41851112b
saramohamed124 <saramohamaden1234@gmail.com>
2025-04-24 23:32:46 +0200

    fix

--- src/app/login/page.js
+++
@@ -1,7 +0,0 @@
-import dynamic from 'next/dynamic';
-
-const LoginClient = dynamic(() => import('./components/LoginClient'), { ssr: false });
-
-export default function LoginPage() {
-  return <LoginClient />;
-}
